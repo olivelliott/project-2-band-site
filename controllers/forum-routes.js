66d2d01b22e5193313b902e563a6699eb57d8c5c
cardinal <caren@merzhaus.org>
2022-07-23 16:14:23 -0400

    cleaned up single post code in forum-routes

--- controllers/forum-routes.js
+++ controllers/forum-routes.js
@@ -9,72 +9,72 @@ const withAuth = require("../utils/auth");
 // RENDER it to the 'forum-home' handlebars page
 
 //! add withAuth once pathways are cleared
-router.get("/",  (req, res) => {
+router.get("/", (req, res) => {
     console.log(req.session);
     console.log("======================");
     Post.findAll({
-        attributes: ["id", "title", "user_id"],
-        include: [{
-                model: Comment,
-                attributes: ["id", "comment_text", "post_id", "user_id"],
-                include: {
+            attributes: ["id", "title", "user_id"],
+            include: [{
+                    model: Comment,
+                    attributes: ["id", "comment_text", "post_id", "user_id"],
+                    include: {
+                        model: User,
+                        attributes: ["username"],
+                    },
+                },
+                {
                     model: User,
                     attributes: ["username"],
                 },
-            },
-            {
-                model: User,
-                attributes: ["username"],
-            },
-        ],
-    })
-    .then((dbPostData) => {
-        const posts = dbPostData.map((post) => post.get({ plain: true }));
-        res.render("forum-home", { posts, loggedIn: true });
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+            ],
+        })
+        .then((dbPostData) => {
+            const posts = dbPostData.map((post) => post.get({ plain: true }));
+            res.render("forum-home", { posts, loggedIn: true });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // * GET SINGLE FORUM POST : include : comment, user
 // RENDER it to the 'single-post' handlebars page
 
-//I think this path is probably wrong
-// // TODO: router.get("/edit/:id", withAuth, (req, res) => {
-//     Post.findByPk(req.params.id, {
-//             attributes: ["id", "username", "title"],
-//             include: [{
-//                     model: Comment,
-//                     attributes: ["id", "comment_text", "post_id", "user_id"],
-//                     include: {
-//                         model: User,
-//                         attributes: ["username"],
-//                     },
-//                 },
-//                 {
-//                     model: User,
-//                     attributes: ["username"],
-//                 },
-//             ],
-//         })
-//         .then((dbPostData) => {
-//             if (dbPostData) {
-//                 const post = dbPostData.get({ plain: true });
+//! add withAuth after the path below once pathways are cleared
 
-//                 res.render("single-post", {
-//                     post,
-//                     loggedIn: true,
-//                 });
-//             } else {
-//                 res.status(404).end();
-//             }
-//         })
-//         .catch((err) => {
-//             res.status(500).json(err);
-//         });
-// });
+router.get("/edit/:id", (req, res) => {
+    Post.findByPk(req.params.id, {
+            attributes: ["id", "title", "user_id"],
+            include: [{
+                    model: Comment,
+                    attributes: ["id", "comment_text", "user_id", "post_id"],
+                    include: {
+                        model: User,
+                        attributes: ["username", "instagram", "facebook"],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ["username"],
+                },
+            ],
+        })
+        .then((dbPostData) => {
+            if (dbPostData) {
+                const post = dbPostData.get({ plain: true });
 
+                res.render("single-post", {
+                    post,
+                    loggedIn: true,
+                });
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
