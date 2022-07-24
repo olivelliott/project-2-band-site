633d53ccdaae4ea07fee50146ecea2e0f4b53c8f
Olive <olivelliott48@gmail.com>
2022-07-23 22:02:58 -0400

    Connect and test gsap

---
+++ public/javascript/gsap.js
@@ -0,0 +1,4 @@
+
+gsap.from('#band-name', {duration: 1.5, opacity: 0, scale: 0.3, ease:'back'} )
+
+gsap.from('.band-pic', {duration: 1.5, opacity: 0, scale: 0.3, ease:'back'});
---
+++ public/javascript/shows.js
@@ -0,0 +1,16 @@
+// async function showHandler(req, res) {
+//     req.session.end();
+//     const showLink = document.getElementById('show-link').href.replace('http://localhost:3001/', '')
+//     // console.log(showLink);
+
+//     showTest(showLink);
+
+
+// }
+
+// function showTest(showLink) {
+//     console.log(showLink)
+//     window.location = showLink;
+// }
+
+// document.querySelector('#show-link').addEventListener('click', showHandler);
