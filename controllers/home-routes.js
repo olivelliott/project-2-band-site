ef7a98099436e65bc650ce251a229b28e7b0b0d2
cardinal <caren@merzhaus.org>
2022-07-27 21:03:16 -0400

    forum changes, and need to get other pull request

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -15,47 +15,37 @@ router.get("/", (req, res) => {
     res.render("homepage");
 });
 
-router.get("/posts/:id", (req, res) => {
-    Post.findByPk(req.params.id, {
-            attributes: ["id", "title", "user_id", "content"],
-            include: [{
-                    model: Comment,
-                    attributes: ["id", "comment_text", "user_id", "post_id"],
-                    include: {
-                        model: User,
-                        attributes: ["username", "instagram", "facebook"],
-                    },
-                },
-                {
-                    model: User,
-                    attributes: ["username"],
-                },
-            ],
-        })
-        .then((dbPostData) => {
-            if (dbPostData) {
-                const post = dbPostData.get({ plain: true });
-                res.render("single-post", {
-                    post,
-                    loggedIn: true,
-                });
-            } else {
-                res.status(404).end();
-            }
-        })
-        .catch((err) => {
-            res.status(500).json(err);
-        });
-});
-
-// GET all comments
-router.get("/", (req, res) => {
-    Comment.findAll()
-        .then((dbCommentData) => res.json(dbCommentData))
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
+// router.get("/posts/:id", (req, res) => {
+//     Post.findByPk(req.params.id, {
+//             attributes: ["id", "title", "user_id", "content"],
+//             include: [{
+//                     model: Comment,
+//                     attributes: ["id", "comment_text", "user_id", "post_id"],
+//                     include: {
+//                         model: User,
+//                         attributes: ["username", "instagram", "facebook"],
+//                     },
+//                 },
+//                 {
+//                     model: User,
+//                     attributes: ["username"],
+//                 },
+//             ],
+//         })
+//         .then((dbPostData) => {
+//             if (dbPostData) {
+//                 const post = dbPostData.get({ plain: true });
+//                 res.render("single-post", {
+//                     post,
+//                     loggedIn: true,
+//                 });
+//             } else {
+//                 res.status(404).end();
+//             }
+//         })
+//         .catch((err) => {
+//             res.status(500).json(err);
+//         });
+// });
 
 module.exports = router;
